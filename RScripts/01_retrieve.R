fd31f7d1c19f8a5b168f510a787ed7b22c2e83b6
lorenzatoma <lorenza.tomassetti@cerved.com>
2025-06-16 18:49:05 +0200

    changes to update db

--- RScripts/01_retrieve.R
+++ RScripts/01_retrieve.R
@@ -18,12 +18,12 @@ if (use_DATE) {
     date_info <- db_get_minmax_dates(con, 'ME01_gme_mgp_offers', 'DATE')
     from_data <- as.Date(date_info$max_date)
   } 
-
+# from_data <- as.Date('2025-06-01')
 
 # Get filename list -----------------------------------
 mgp_offers_files = gme_offers_get_files(data_type = 'MGP', output_dir = output_dir, username = username, password = password)
 
-print('[01/04]')
+print('[01/07]')
 
 
 # Extract the last n elements
@@ -94,8 +94,9 @@ if (use_DATE) {
 # Get filename list -----------------------------------
 msd_offers_files = gme_offers_get_files(data_type = 'MSD', output_dir = output_dir, username = username, password = password)
 
-print('[02/04]')
+print('[02/07]')
 
+# max(as.Date(substr(msd_offers_files, 1, 8), format = "%Y%m%d"), na.rm = TRUE)
 
 # Extract the last n elements
 last_files = msd_offers_files[as.Date(substr(msd_offers_files, 1, 8), format = "%Y%m%d") > from_data]
@@ -161,6 +162,7 @@ if (use_DATE) {
   date_info <- db_get_minmax_dates(con, 'ME01_gme_mb_offers', 'DATE')
   from_data <- as.Date(date_info$max_date)
 } 
+# from_data <- as.Date('2025-04-27')
 
 # Get filename list -----------------------------------
 mb_offers_files = gme_offers_get_files(data_type = 'MB', output_dir = output_dir, username = username, password = password)
@@ -226,132 +228,303 @@ if(isFALSE(check_load) & isFALSE(check_files)) {
  
  
 # XBID --------------------------------------------------------------
-#  
-# if (use_DATE) {
-#   # Step 1: Get min and max dates
-#   date_info <- db_get_minmax_dates(con, 'ME01_gme_xbid_offers', 'DATE')
-#   from_data <- as.Date(date_info$max_date)
-# } 
-# 
-# # Get filename list -----------------------------------
-# xbid_offers_files = gme_offers_get_files(data_type = 'XBID', output_dir = output_dir, username = username, password = password)
-# 
-# print('[04/04]')
-# 
-# 
-# # Extract the last n elements
-# last_files = xbid_offers_files[as.Date(substr(xbid_offers_files, 1, 8), format = "%Y%m%d") > from_data]
-# if (!exists("last_files") || length(last_files) == 0 || all(is.na(last_files))) {last_files = character(0)}
-# 
-# list_xbid_offers <- lapply(last_files, function(file) {
-#     tryCatch({
-#         # Call gme_download_offers_file with explicit arguments
-#         gme_download_offers_file(
-#             filename = file,  
-#             data_type = 'XBID',
-#             output_dir = output_dir,
-#             username = username,       # FTP username for authentication
-#             password = password,       # FTP password for authentication
-#             raw = FALSE
-#         )
-#     }, error = function(e) {
-#         # In case of an error (e.g., failed download or processing), return NULL
-#         message("Error processing file: ", file, " - ", e$message)
-#         return(NULL)
-#     })
-# })
-#  
-# dt_xbid_offers = rbindlist(list_xbid_offers, fill=TRUE)
-# dt_xbid_offers[bid_offer_date_dt_parsed := as.IDate(sub("T.*", "", DATETIME))]
-# # check 
-# check_load = (nrow(dt_xbid_offers) > 0)
-# check_files = (is.character(last_files) & !is.null(last_files) & length(last_files) == 0)
-# 
-# if(isTRUE(check_load)) {
-#   print(glue("{crayon::green('ME01_gme_mgp_prices retrieved')}"))
-#   dt_all_raw[['ME01_gme_mgp_prices']] = dt_xbid_offers[bid_offer_date_dt_parsed > from_data]
-#   new_data_tables = rbind(new_data_tables, data.table::data.table(
-#     table_name = 'ME01_gme_mgp_prices',
-#     num_rows = 0,
-#     type = 'NEW_DATA'
-#   ))
-# } 
-# 
-# if(isFALSE(check_load) & isTRUE(check_files)) {
-#   print(glue("{crayon::yellow('ME01_gme_mgp_prices no new data')}"))
-#   nonew_data_tables = rbind(nonew_data_tables, data.table::data.table(
-#     table_name = 'ME01_gme_mgp_prices',
-#     num_rows = 0,
-#     type = 'NO_NEW_DATA'
-#   ))
-# } 
-# 
-# if(isFALSE(check_load) & isFALSE(check_files)) {
-#   print(glue("{crayon::red('ME01_gme_mgp_prices error')}"))
-#   failed_tables_retrieve = rbind(failed_tables_retrieve, data.table::data.table(
-#     table_name = 'ME01_gme_mgp_prices',
-#     num_rows = 0,
-#     type = 'ERROR_RETRIEVE'
-#   ))
-# }
+
+if (use_DATE) {
+  # Step 1: Get min and max dates
+  date_info <- db_get_minmax_dates(con, 'ME01_gme_xbid_offers', 'DATE')
+  from_data <- as.Date(date_info$max_date)
+}
+
+from_data <- as.Date('2022-12-31')
+# Get filename list -----------------------------------
+xbid_offers_files = gme_offers_get_files(data_type = 'XBID', output_dir = output_dir, username = username, password = password)
+
+print('[04/07]')
+
+
+# Extract the last n elements
+last_files = xbid_offers_files[as.Date(substr(xbid_offers_files, 1, 8), format = "%Y%m%d") > from_data]
+if (!exists("last_files") || length(last_files) == 0 || all(is.na(last_files))) {last_files = character(0)}
+
+list_xbid_offers <- lapply(last_files, function(file) {
+    tryCatch({
+        # Call gme_download_offers_file with explicit arguments
+        gme_download_offers_file(
+            filename = file,
+            data_type = 'XBID',
+            output_dir = output_dir,
+            username = username,       # FTP username for authentication
+            password = password,       # FTP password for authentication
+            raw = FALSE
+        )
+    }, error = function(e) {
+        # In case of an error (e.g., failed download or processing), return NULL
+        message("Error processing file: ", file, " - ", e$message)
+        return(NULL)
+    })
+})
+
+dt_xbid_offers = rbindlist(list_xbid_offers, fill=TRUE)
+dt_xbid_offers[, bid_offer_date_dt_parsed := as.IDate(sub("T.*", "", timestamp))]
+saveRDS(dt_xbid_offers, file = "dt_xbid_offers.rds")
+dt_xbid_offers <- as.data.table(readRDS("dt_xbid_offers.rds"))
+
+
+
+# check
+check_load = (nrow(dt_xbid_offers) > 0)
+check_files = (is.character(last_files) & !is.null(last_files) & length(last_files) == 0)
+dt_all_raw[['ME01_gme_xbid_offers']] = dt_xbid_offers
+
+if(isTRUE(check_load)) {
+  print(glue("{crayon::green('ME01_gme_xbid_offers retrieved')}"))
+  dt_all_raw[['ME01_gme_xbid_offers']] = dt_xbid_offers[bid_offer_date_dt_parsed > from_data]
+  new_data_tables = rbind(new_data_tables, data.table::data.table(
+    table_name = 'ME01_gme_xbid_offers',
+    num_rows = 0,
+    type = 'NEW_DATA'
+  ))
+}
+
+if(isFALSE(check_load) & isTRUE(check_files)) {
+  print(glue("{crayon::yellow('ME01_gme_xbid_offers no new data')}"))
+  nonew_data_tables = rbind(nonew_data_tables, data.table::data.table(
+    table_name = 'ME01_gme_xbid_offers',
+    num_rows = 0,
+    type = 'NO_NEW_DATA'
+  ))
+}
+
+if(isFALSE(check_load) & isFALSE(check_files)) {
+  print(glue("{crayon::red('ME01_gme_xbid_offers error')}"))
+  failed_tables_retrieve = rbind(failed_tables_retrieve, data.table::data.table(
+    table_name = 'ME01_gme_xbid_offers',
+    num_rows = 0,
+    type = 'ERROR_RETRIEVE'
+  ))
+}
+
  
- 
+
 # MI-A1 --------------------------------------------------------------
- 
-# data_type <- 'MGP_Transiti'
-# username <- "PIASARACENO"
-# password <- "18N15C9R"
-# output_dir = "data"
+if (use_DATE) {
+  # Step 1: Get min and max dates
+  date_info <- db_get_minmax_dates(con, 'ME01_gme_mia1_offers', 'DATE')
+  from_data <- as.Date(date_info$max_date)
+}
 
-# # Get filename list -----------------------------------
-# mgp_tran_files = gme_offers_get_files(data_type = data_type, output_dir = output_dir, username = username, password = password)
+from_data <- as.Date('2022-12-31')
 
-# # check 
-# check_dt_05 = all(!is.null(mgp_tran_files))
+# from_data <- as.Date('2025-06-01')
+# Get filename list -----------------------------------
+mia1_offers_files = gme_offers_get_files(data_type = 'MI-A1', output_dir = output_dir, username = username, password = password)
+
+print('[05/07]')
 
 
-# print('[05/11]')
+# Extract the last n elements
+last_files = mia1_offers_files[as.Date(substr(mia1_offers_files, 1, 8), format = "%Y%m%d") > from_data]
+if (!exists("last_files") || length(last_files) == 0 || all(is.na(last_files))) {last_files = character(0)}
+
+list_mia1_offers <- lapply(last_files, function(file) {
+  tryCatch({
+    # Call gme_download_offers_file with explicit arguments
+    gme_download_offers_file(
+      filename = file,
+      data_type = 'MI-A1',
+      output_dir = output_dir,
+      username = username,       # FTP username for authentication
+      password = password,       # FTP password for authentication
+      raw = FALSE
+    )
+  }, error = function(e) {
+    # In case of an error (e.g., failed download or processing), return NULL
+    message("Error processing file: ", file, " - ", e$message)
+    return(NULL)
+  })
+})
+
+dt_mia1_offers = rbindlist(list_mia1_offers, fill=TRUE)
+saveRDS(dt_mia1_offers, file = "dt_mia1_offers.rds")
+dt_mia1_offers <- as.data.table(readRDS("dt_mia1_offers.rds"))
 
 
-# # Extract the last n elements
-# last_files <- tail(mgp_tran_files, n)
-# print(last_files)
 
-# list_mgp_tran <- lapply(last_files, function(file) {
-#     tryCatch({
-#         # Call gme_download_offers_file with explicit arguments
-#         gme_download_offers_file(
-#             filename = file,  
-#             data_type = data_type,
-#             output_dir = output_dir,
-#             username = username,       # FTP username for authentication
-#             password = password,       # FTP password for authentication
-#             raw = FALSE
-#         )
-#     }, error = function(e) {
-#         # In case of an error (e.g., failed download or processing), return NULL
-#         message("Error processing file: ", file, " - ", e$message)
-#         return(NULL)
-#     })
-# })
- 
-# dt_mgp_tran = rbindlist(list_mgp_tran)
+# check
+check_load = (nrow(dt_mia1_offers) > 0)
+check_files = (is.character(last_files) & !is.null(last_files) & length(last_files) == 0)
 
-# # check 
-# check_dt_04 = all(!is.null(dt_mgp_tran))
+if(isTRUE(check_load)) {
+  print(glue("{crayon::green('ME01_gme_mia1_offers retrieved')}"))
+  dt_all_raw[['ME01_gme_mia1_offers']] = dt_mia1_offers[bid_offer_date_dt_parsed > from_data]
+  new_data_tables = rbind(new_data_tables, data.table::data.table(
+    table_name = 'ME01_gme_mia1_offers',
+    num_rows = 0,
+    type = 'NEW_DATA'
+  ))
+}
 
-# if(isTRUE(check_dt_04)) {
-#     dt_all_raw[['dt_mgp_tran']] = dt_mgp_tran
-# } else {
-#     failed_tables_retrieve <- rbind(failed_tables_retrieve, data.table::data.table(
-#     table_name = 'dt_mgp_tran',
-#     num_rows = 0,
-#     type = 'ERROR_RETRIEVE'
-#     ))
-# }
-# print('[05/11]') 
- 
- 
+if(isFALSE(check_load) & isTRUE(check_files)) {
+  print(glue("{crayon::yellow('ME01_gme_mia1_offers no new data')}"))
+  nonew_data_tables = rbind(nonew_data_tables, data.table::data.table(
+    table_name = 'ME01_gme_mia1_offers',
+    num_rows = 0,
+    type = 'NO_NEW_DATA'
+  ))
+}
+
+if(isFALSE(check_load) & isFALSE(check_files)) {
+  print(glue("{crayon::red('ME01_gme_mia1_offers error')}"))
+  failed_tables_retrieve = rbind(failed_tables_retrieve, data.table::data.table(
+    table_name = 'ME01_gme_mia1_offers',
+    num_rows = 0,
+    type = 'ERROR_RETRIEVE'
+  ))
+}
+
+
+
+# MI-A2 --------------------------------------------------------------
+if (use_DATE) {
+  # Step 1: Get min and max dates
+  date_info <- db_get_minmax_dates(con, 'ME01_gme_mia2_offers', 'DATE')
+  from_data <- as.Date(date_info$max_date)
+}
+
+# from_data <- as.Date('2025-06-01')
+# Get filename list -----------------------------------
+mia2_offers_files = gme_offers_get_files(data_type = 'MI-A2', output_dir = output_dir, username = username, password = password)
+
+print('[06/07]')
+
+
+# Extract the last n elements
+last_files = mia2_offers_files[as.Date(substr(mia2_offers_files, 1, 8), format = "%Y%m%d") > from_data]
+if (!exists("last_files") || length(last_files) == 0 || all(is.na(last_files))) {last_files = character(0)}
+
+list_mia2_offers <- lapply(last_files, function(file) {
+  tryCatch({
+    # Call gme_download_offers_file with explicit arguments
+    gme_download_offers_file(
+      filename = file,
+      data_type = 'MI-A2',
+      output_dir = output_dir,
+      username = username,       # FTP username for authentication
+      password = password,       # FTP password for authentication
+      raw = FALSE
+    )
+  }, error = function(e) {
+    # In case of an error (e.g., failed download or processing), return NULL
+    message("Error processing file: ", file, " - ", e$message)
+    return(NULL)
+  })
+})
+
+dt_mia2_offers = rbindlist(list_mia2_offers, fill=TRUE)
+
+# check
+check_load = (nrow(dt_mia1_offers) > 0)
+check_files = (is.character(last_files) & !is.null(last_files) & length(last_files) == 0)
+
+if(isTRUE(check_load)) {
+  print(glue("{crayon::green('ME01_gme_mia2_offers retrieved')}"))
+  dt_all_raw[['ME01_gme_mia2_offers']] = dt_mia2_offers[bid_offer_date_dt_parsed > from_data]
+  new_data_tables = rbind(new_data_tables, data.table::data.table(
+    table_name = 'ME01_gme_mia2_offers',
+    num_rows = 0,
+    type = 'NEW_DATA'
+  ))
+}
+
+if(isFALSE(check_load) & isTRUE(check_files)) {
+  print(glue("{crayon::yellow('ME01_gme_mia2_offers no new data')}"))
+  nonew_data_tables = rbind(nonew_data_tables, data.table::data.table(
+    table_name = 'ME01_gme_mia2_offers',
+    num_rows = 0,
+    type = 'NO_NEW_DATA'
+  ))
+}
+
+if(isFALSE(check_load) & isFALSE(check_files)) {
+  print(glue("{crayon::red('ME01_gme_mia2_offers error')}"))
+  failed_tables_retrieve = rbind(failed_tables_retrieve, data.table::data.table(
+    table_name = 'ME01_gme_mia2_offers',
+    num_rows = 0,
+    type = 'ERROR_RETRIEVE'
+  ))
+}
+
+
+
+# MI-A3 --------------------------------------------------------------
+if (use_DATE) {
+  # Step 1: Get min and max dates
+  date_info <- db_get_minmax_dates(con, 'ME01_gme_mia3_offers', 'DATE')
+  from_data <- as.Date(date_info$max_date)
+}
+
+# from_data <- as.Date('2025-06-01')
+# Get filename list -----------------------------------
+mia3_offers_files = gme_offers_get_files(data_type = 'MI-A3', output_dir = output_dir, username = username, password = password)
+
+print('[07/07]')
+
+
+# Extract the last n elements
+last_files = mia3_offers_files[as.Date(substr(mia3_offers_files, 1, 8), format = "%Y%m%d") > from_data]
+if (!exists("last_files") || length(last_files) == 0 || all(is.na(last_files))) {last_files = character(0)}
+
+list_mia3_offers <- lapply(last_files, function(file) {
+  tryCatch({
+    # Call gme_download_offers_file with explicit arguments
+    gme_download_offers_file(
+      filename = file,
+      data_type = 'MI-A3',
+      output_dir = output_dir,
+      username = username,       # FTP username for authentication
+      password = password,       # FTP password for authentication
+      raw = FALSE
+    )
+  }, error = function(e) {
+    # In case of an error (e.g., failed download or processing), return NULL
+    message("Error processing file: ", file, " - ", e$message)
+    return(NULL)
+  })
+})
+
+dt_mia3_offers = rbindlist(list_mia3_offers, fill=TRUE)
+
+# check
+check_load = (nrow(dt_mia1_offers) > 0)
+check_files = (is.character(last_files) & !is.null(last_files) & length(last_files) == 0)
+
+if(isTRUE(check_load)) {
+  print(glue("{crayon::green('ME01_gme_mia3_offers retrieved')}"))
+  dt_all_raw[['ME01_gme_mia3_offers']] = dt_mia3_offers[bid_offer_date_dt_parsed > from_data]
+  new_data_tables = rbind(new_data_tables, data.table::data.table(
+    table_name = 'ME01_gme_mia3_offers',
+    num_rows = 0,
+    type = 'NEW_DATA'
+  ))
+}
+
+if(isFALSE(check_load) & isTRUE(check_files)) {
+  print(glue("{crayon::yellow('ME01_gme_mia3_offers no new data')}"))
+  nonew_data_tables = rbind(nonew_data_tables, data.table::data.table(
+    table_name = 'ME01_gme_mia3_offers',
+    num_rows = 0,
+    type = 'NO_NEW_DATA'
+  ))
+}
+
+if(isFALSE(check_load) & isFALSE(check_files)) {
+  print(glue("{crayon::red('ME01_gme_mia3_offers error')}"))
+  failed_tables_retrieve = rbind(failed_tables_retrieve, data.table::data.table(
+    table_name = 'ME01_gme_mia3_offers',
+    num_rows = 0,
+    type = 'ERROR_RETRIEVE'
+  ))
+}
 
 # EXPORT RAW ===========================================================================
 
